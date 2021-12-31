46dae6aa6ec10b10ea46e8d95f1b9dc3b7fd8499
AngelesCahill <70714713+AngelesCahill@users.noreply.github.com>
2021-12-31 19:33:43 -0300

    agregando ntfl

--- dist/js/Contacto.5162656b.js
+++ dist/js/Contacto.5162656b.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["Contacto"],{c098:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"container"},[e("div",{staticClass:"row"},[e("h1",[t._v("Envianos un mensaje y te contactaremos")])])])}],s={name:"Contacto"},o=s,r=e("2877"),i=Object(r["a"])(o,a,c,!1,null,null,null);n["default"]=i.exports}}]);
-//# sourceMappingURL=Contacto.18292dc1.js.map
+//# sourceMappingURL=Contacto.5162656b.js.map
---
+++ index.js
@@ -0,0 +1,40 @@
+const { fork } = require('child_process')
+const path = require('path')
+const lambdaBuild = require('netlify-lambda/lib/build')
+
+module.exports = (api, projectOptions) => {
+    const { build, serve } = api.service.commands;
+    const buildFn = build.fn;
+    const serveFn = serve.fn;
+    const webpackConfig = projectOptions.pluginOptions && projectOptions.pluginOptions.netlify && projectOptions.pluginOptions.netlify.webpackConfig;
+
+    build.fn = async (...args) => {
+        try {
+            const [res, stats] = await Promise.all([
+                buildFn(...args),
+                lambdaBuild.run('src/lambda', webpackConfig),
+            ]);
+            console.log(stats.toString({ color: true }));
+            return res;
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    }
+
+    serve.fn = (...args) => {
+        const devServer = api.service.projectOptions.devServer = api.service.projectOptions.devServer || {}
+        if (!devServer.proxy) {
+            devServer.proxy = {}
+        }
+        devServer.proxy['/.netlify/functions'] = {
+            "target": "http://localhost:9000",
+            "pathRewrite": {
+                "^/\\.netlify/functions": ""
+            }
+        }
+
+        fork(require.resolve('netlify-lambda'), ['serve', 'src/lambda', ...(webpackConfig ? ['-c', webpackConfig] : [])]);
+        return serveFn(...args)
+    }
+}
